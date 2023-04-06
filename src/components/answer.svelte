c7e48844cbcc9361590a60a0b55b41c67e249575
Calvin Bonner <creb8u@gmail.com>
2023-04-06 19:18:54 +0000

    Added styles

--- src/components/answer.svelte
+++ src/components/answer.svelte
@@ -3,7 +3,22 @@
 </script>
 
 {#if answer}
-	<p style="color: green;">Good job!</p>
+	<p class="correct">Good job!</p>
 {:else}
-	<p style="color: red;">Not quite...</p>
+	<p class="incorrect">Not quite...</p>
 {/if}
+
+<style>
+	.correct, .incorrect {
+		font-weight: bold;
+		text-decoration: underline;
+	}
+
+	.correct {
+		color: green;
+	}
+
+	.incorrect {
+		color: red;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<div class="background">
+    <div class="layout">
+        <slot />
+    </div>
+</div>
+
+<style>
+    .background {
+        padding: 32px;
+
+        position: absolute;
+        inset: 0;
+
+        background-color: #abacc2;
+    }
+
+    .layout {
+        width: 100%;
+        max-width: 900px;
+        margin: 0 auto;
+    }
+</style>
