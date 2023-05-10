ee0dd537ae3ee6af97c5755791cf10ff37491390
savannah remijn <savannahremijn@gmail.com>
2023-05-10 20:55:20 +0000

    Question changes

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -8,7 +8,7 @@
  */
 export const questions = [
     {
-        question: "This is the first question.",
+        question: "This class has been a great class",
         answers: [
             "True",
             "False"
@@ -17,16 +17,118 @@ export const questions = [
         time: 20
     },
     {
-        question: "Who made this question?",
+        question: "Was there a great Biblical principle each week?",
         answers: [
-            "Ben Thomas",
-            "Mr. Bonner",
-            "Sam Mote",
-            "Camryn Verhey"
+            " Yes, every week",
+            "Never",
+            "Sometimes"
+        ],
+        correct: 0,
+        time: 20
+    },
+    {
+        question: "Mr Bonner is a faster typer than any of his students",
+        answers: [
+            "True",
+            "False"
+        ],
+        correct: 0,
+        time: 20
+    },
+    {
+        question: "What is Google?",
+        answers: [
+            "The answer to all of our problems",
+            "The most popular search engine in the world", 
+            "A scam"
         ],
         correct: 1,
         time: 20
     },
+
+    {
+        question: "Micorsoft word is...",
+        answers: [
+            "A word processer",
+            "A presentation software",
+            "A datatool"
+        ],
+        correct: 0,
+        time: 20
+    },
+
+    {
+        question: "When presenting to a group of people you should..",
+        answers: [
+            "Look at the ground the whole time",
+            "Talk super loud so people hear you",
+            "Articulate clearly and engage your audience" 
+        ],
+        correct: 2,
+        time: 20
+    },
+    {
+        question: "Hardware is the physical component of a computer",
+        answers: [
+            "True",
+            "False"
+        ],
+        correct: 0,
+        time: 20
+    },
+    {
+        question: "If your computer is not preforming the task you want it to, you should..",
+        answers: [
+            "Throw it on the ground",
+            "Buy a new one", 
+            "Troubleshoot"
+        ],
+        correct: 2,
+        time: 20
+    },
+
+    {
+        question: "Troubleshooting is..",
+        answers: [
+            "A systimatic approach to solving a problem",
+            "Hoping for the best when trying to solve a problem",
+            "Guessing"
+        ],
+        correct: 0,
+        time: 20
+    },
+
+    {
+        question: "Photo editing can be a great way to express creativity",
+        answers: [
+            "True",
+            "False"
+        ],
+        correct: 0,
+        time: 20
+    },
+
+    {
+        question: "What are the benefits of working in a group?",
+        answers: [
+            "None",
+            "Letting other people do all the work",
+            "Finding new ways to look at an issue and learning how to work with people who are different than ourselves "
+        ],
+        correct: 2,
+        time: 20
+    },
+
+    {
+        question: "Writting code is super easy",
+        answers: [
+            "True",
+            "False"
+        ],
+        correct: 1,
+        time: 20
+    },
+
     {
         question: "How much time is left in this class?",
         answers: [
@@ -36,5 +138,14 @@ export const questions = [
         ],
         correct: 2,
         time: 20
-    }
+    }, 
+    {
+        question: "Will Savannah be at the beach thinking about how much she misses this class all summer? ",
+        answers: [
+            "Yes, she will be daydreaming about this class while boating all summer",
+            "No, she will be inside writing code all summer"
+        ],
+        correct: 0,
+        time: 20
+    },
 ]
